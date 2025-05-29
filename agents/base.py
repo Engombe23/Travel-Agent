8b288620609bdf37540a0f5b5c0c8353699238ff
Engombe Lokanga <engombeel13@gmail.com>
2025-05-29 12:38:49 +0100

    Added model to base class as each agent is using the same LLM

--- agents/base.py
+++ agents/base.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 class Agent(ABC):
+
+  def __init__(self, api_key: str):
+    self.llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", api_key=api_key)
 
   @abstractmethod
   def run(self, input_data: dict) -> dict:
