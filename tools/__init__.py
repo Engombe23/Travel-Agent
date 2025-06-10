60399d512271e4ca3469f1f7b4de3cfacb67ab42
Engombe Lokanga <engombeel13@gmail.com>
2025-06-10 12:19:52 +0100

    Minor changes

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,6 +1,5 @@
 from .activity_tool import plan_activity
 from .flight_tool import plan_flight
-from .holiday_planner_tool import plan_holiday
 from .hotel_tool import plan_hotel
 
-__all__ = ['plan_activity', 'plan_flight', 'plan_holiday', 'plan_hotel']
+__all__ = ['plan_activity', 'plan_flight', 'plan_hotel']
